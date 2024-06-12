20de341afafdbde1b4b504648db0a23b2d53d03c
ARamos <albertramos902@gmail.com>
2024-06-12 11:55:13 +0100

    added responsive layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <h1>Hello Worls</h1>
+    <Link href="#">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos nemo, incidunt nostrum sit vero error laborum voluptate optio veritatis reprehenderit, sapiente nobis! Aliquam qui laudantium animi, tempora ad nihil tempore?</Link>
+  </div>;
 }
