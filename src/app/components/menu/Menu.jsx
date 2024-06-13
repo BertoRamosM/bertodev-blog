26a5f263bd3840cdd615d823e28b16c58e918c1c
ARamos <albertramos902@gmail.com>
2024-06-13 21:00:42 +0100

    finished side menu and leave for the day, see you tomorrow

--- src/app/components/menu/Menu.jsx
+++ src/app/components/menu/Menu.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import style from "./menu.module.css";
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Menu = () => {
   return (
     <div className={style.container}>
-    menu
-  </div>)
+      <h2 className={style.subtitle}>{"What's hot"}</h2>
+      <h1 className={style.title}>Most Popular</h1>
+      <div className={style.items}>
+        <Link href="/" className={style.item}>
+          <div className={style.imageContainer}>
+            <Image src="/code.jpg" alt="" fill className={style.image} />
+          </div>
+          <div className={style.textContainer}>
+            <span className={`${style.category} ${style.personal}`}>
+              Personal
+            </span>
+            <h3 className={style.titleDesc}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Inventore, earum?
+            </h3>
+            <div className={style.detail}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}>13.06.2024</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
