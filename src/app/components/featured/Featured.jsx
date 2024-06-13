44352bd94c4c050a68ebfb1087b720e595134183
ARamos <albertramos902@gmail.com>
2024-06-13 15:56:29 +0100

    creating hero component

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import style from "./featured.module.css";
+import Image from 'next/image';
 
 const Featured = () => {
-  return <div className={style.container}>Featured</div>;
+  return (
+    <div className={style.container}>
+      <h1 className={style.title}>
+        Hey <b>Alberto Ramos </b>here!
+      </h1>
+
+      <div className={style.post}>
+        <div className={style.imgContainer}>
+          <Image src="/code.jpg" alt="code picture" fill className={style.image} />
+        </div>
+        <div className={style.textContainer}>
+          <h2 className={style.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h2>
+          <p className={style.postDesc}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            blanditiis velit est obcaecati, vitae nihil! Iure, harum dolorem.
+            Et, cum sequi? Quas dolor reprehenderit quasi perspiciatis,
+            quibusdam illum dolores aut.
+          </p>
+
+          <button className={style.postButton}>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Featured;
