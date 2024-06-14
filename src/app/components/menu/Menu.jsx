18d910ee853d08458fb5ed7bcd6214a75a23b18e
ARamos <albertramos902@gmail.com>
2024-06-14 10:52:05 +0100

    created hottest blog posts secton

--- src/app/components/menu/Menu.jsx
+++ src/app/components/menu/Menu.jsx
@@ -10,6 +10,101 @@ const Menu = () => {
       <h1 className={style.title}>Most Popular</h1>
       <div className={style.items}>
         <Link href="/" className={style.item}>
+          <div className={style.imageContainer}></div>
+          <div className={style.textContainer}>
+            <span className={`${style.category} ${style.personal}`}>
+              Personal
+            </span>
+            <h3 className={style.titleDesc}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Inventore, earum?
+            </h3>
+            <div className={style.detail}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}> - 13.06.2024</span>
+            </div>
+          </div>
+        </Link>
+
+        <Link href="/" className={style.item}>
+          <div className={style.imageContainer}></div>
+          <div className={style.textContainer}>
+            <span className={`${style.category} ${style.dev}`}>Dev</span>
+            <h3 className={style.titleDesc}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Inventore, earum?
+            </h3>
+            <div className={style.detail}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}> - 13.06.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={style.item}>
+          <div className={style.imageContainer}></div>
+          <div className={style.textContainer}>
+            <span className={`${style.category} ${style.projects}`}>
+              Projects
+            </span>
+            <h3 className={style.titleDesc}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Inventore, earum?
+            </h3>
+            <div className={style.detail}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}> - 13.06.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={style.item}>
+          <div className={style.imageContainer}></div>
+          <div className={style.textContainer}>
+            <span className={`${style.category} ${style.hacks}`}>Hacks</span>
+            <h3 className={style.titleDesc}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Inventore, earum?
+            </h3>
+            <div className={style.detail}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}> - 13.06.2024</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      <h2 className={style.subtitle}>Discover by topics</h2>
+      <h1 className={style.title}>Categories</h1>
+      <div className={style.categoryList}>
+        <Link
+          href="/blog?cat=personal"
+          className={`${style.category} ${style.personal}`}
+        >
+          Personal
+        </Link>
+        <Link
+          href="/blog?cat=dev"
+          className={`${style.category} ${style.dev}`}
+        >
+          Dev
+        </Link>
+        <Link
+          href="/blog?cat=projects"
+          className={`${style.category} ${style.projects}`}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/blog?cat=hacks"
+          className={`${style.category} ${style.hacks}`}
+        >
+          Hacks
+        </Link>
+      </div>
+
+      <h2 className={style.subtitle}>Chosen by the editor</h2>
+      <h1 className={style.title}>{`Editor's pick`}</h1>
+      <div className={style.items}>
+        <Link href="/" className={style.item}>
           <div className={style.imageContainer}>
             <Image src="/code.jpg" alt="" fill className={style.image} />
           </div>
@@ -23,7 +118,58 @@ const Menu = () => {
             </h3>
             <div className={style.detail}>
               <span className={style.username}>John Doe</span>
-              <span className={style.date}>13.06.2024</span>
+              <span className={style.date}> - 13.06.2024</span>
+            </div>
+          </div>
+        </Link>
+
+        <Link href="/" className={style.item}>
+          <div className={style.imageContainer}>
+            <Image src="/code.jpg" alt="" fill className={style.image} />
+          </div>
+          <div className={style.textContainer}>
+            <span className={`${style.category} ${style.dev}`}>Dev</span>
+            <h3 className={style.titleDesc}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Inventore, earum?
+            </h3>
+            <div className={style.detail}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}> - 13.06.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={style.item}>
+          <div className={style.imageContainer}>
+            <Image src="/code.jpg" alt="" fill className={style.image} />
+          </div>
+          <div className={style.textContainer}>
+            <span className={`${style.category} ${style.projects}`}>
+              Projects
+            </span>
+            <h3 className={style.titleDesc}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Inventore, earum?
+            </h3>
+            <div className={style.detail}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}> - 13.06.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={style.item}>
+          <div className={style.imageContainer}>
+            <Image src="/code.jpg" alt="" fill className={style.image} />
+          </div>
+          <div className={style.textContainer}>
+            <span className={`${style.category} ${style.hacks}`}>Hacks</span>
+            <h3 className={style.titleDesc}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Inventore, earum?
+            </h3>
+            <div className={style.detail}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}> - 13.06.2024</span>
             </div>
           </div>
         </Link>
