215f51ffaa7aa479a949ed36b1c926d91cdf173c
ARamos <albertramos902@gmail.com>
2024-06-14 15:54:15 +0100

    adding responsiveness to single page

--- src/app/components/comments/Comments.jsx
+++ src/app/components/comments/Comments.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from "./comments.module.css"
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Comments = () => {
   const status = true
@@ -18,6 +19,69 @@ const Comments = () => {
       ) : (
         <Link href="/login">Login to write a comment</Link>
       )}
+      <div className={style.comments}>
+        <div className={style.comment}>
+          <div className={style.user}>
+            <Image
+              className={style.image}
+              src="/code.jpg"
+              alt="avatar"
+              width={50}
+              height={50}
+            />
+            <div className={style.userInfo}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}>01.01.2024</span>
+            </div>
+          </div>
+          <p className={style.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            delectus iste nam ipsam eius eos voluptas deserunt tempora labore
+            totam.
+          </p>
+        </div>
+
+        <div className={style.comment}>
+          <div className={style.user}>
+            <Image
+              className={style.image}
+              src="/code.jpg"
+              alt="avatar"
+              width={50}
+              height={50}
+            />
+            <div className={style.userInfo}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}>01.01.2024</span>
+            </div>
+          </div>
+          <p className={style.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            delectus iste nam ipsam eius eos voluptas deserunt tempora labore
+            totam.
+          </p>
+        </div>
+        <div className={style.comment}>
+          <div className={style.user}>
+            <Image
+              className={style.image}
+              src="/code.jpg"
+              alt="avatar"
+              width={50}
+              height={50}
+            />
+            <div className={style.userInfo}>
+              <span className={style.username}>John Doe</span>
+              <span className={style.date}>01.01.2024</span>
+            </div>
+          </div>
+          <p className={style.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            delectus iste nam ipsam eius eos voluptas deserunt tempora labore
+            totam.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
