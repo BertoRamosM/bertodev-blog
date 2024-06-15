c6b7521aa045341e09b1fcb7f7d9b29b2a20a2af
ARamos <albertramos902@gmail.com>
2024-06-15 11:45:05 +0100

    installed prisma

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,14 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+
+
+const prisma = new PrismaClient();
+
+
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
