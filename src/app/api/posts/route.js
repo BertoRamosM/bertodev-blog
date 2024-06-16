4b64eebf835ba7e63ae4d1b4a68ad7e721ff696f
ARamos <albertramos902@gmail.com>
2024-06-16 21:33:01 +0100

    only fetch category when category its selected

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
 
-  const {searchParams} = new URL(req.url)
+  const { searchParams } = new URL(req.url)
 
   const page = parseInt(searchParams.get("page")) || 1;
 
@@ -11,15 +11,20 @@ export const GET = async (req) => {
   
   const POST_PER_PAGE = 2;
 
+  const query = {
+    take: POST_PER_PAGE,
+    skip: POST_PER_PAGE * (page - 1),
+    where: {
+      ...(cat && { catSlug: cat }),
+    }
+  }
+
   try {
-    const totalPosts = await prisma.post.count();
+    const totalPosts = await prisma.post.count({where: query.where});
       // Calculate the last page
     const lastPage = Math.ceil(totalPosts / POST_PER_PAGE);
     
-    const posts = await prisma.post.findMany(
-     { take: POST_PER_PAGE,
-      skip: POST_PER_PAGE * (page -1)}
-    );
+    const posts = await prisma.post.findMany(query);
     return new NextResponse(JSON.stringify({ posts, totalPosts, lastPage }), {
       status: 200,
     });
