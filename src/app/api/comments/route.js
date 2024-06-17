66ab2471f7d6b0f7e1300606c2053051c6a6ae14
ARamos <albertramos902@gmail.com>
2024-06-17 09:53:12 +0100

    created end point to fetch the comments based on post slug

---
+++ src/app/api/comments/route.js
@@ -0,0 +1,31 @@
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+export const GET = async (req)=> {
+  
+  const { searchParams } = new URL(req.url)
+  const postSlug = searchParams.get('postSlug')
+
+  try {
+    const comments = await prisma.comment.findMany({
+      where: { 
+        ...GET(postSlug && {postSlug})
+       },
+      include:{user: true}
+    })
+
+     
+    
+    return new NextResponse(
+      JSON.stringify(comments, {
+        status: 200,
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong" }),
+      { status: 500 }
+    );
+  }
+};
