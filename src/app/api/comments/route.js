606aa3a83ab8d9519872de4e185f882367e028c7
ARamos <albertramos902@gmail.com>
2024-06-17 10:33:27 +0100

    created end point for creating comments

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
@@ -29,3 +30,41 @@ export const GET = async (req)=> {
     );
   }
 };
+
+
+
+
+//create comment
+export const POST = async (req) => {
+  const session = getAuthSession()
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not authenticated" }),
+      { status: 401 }
+    );
+  }
+  const { searchParams } = new URL(req.url);
+  const postSlug = searchParams.get("postSlug");
+
+  try {
+    const body = await req.JSON()
+    const comment = await prisma.comment.create({
+      data: {
+        ...body, userEmail:session.user.email
+      }
+    })
+    return new NextResponse(
+      JSON.stringify(comment, {
+        status: 200,
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong" }),
+      { status: 500 }
+    );
+  }
+};
+
